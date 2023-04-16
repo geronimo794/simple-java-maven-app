fec28e0dc9a3b3d28dd75cf8153e4cb58e1cc569
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-17 00:34:07 +0700

    test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,12 +1,27 @@
 package hello;
 
 import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
+
+    SpringApplication.run(HelloWorld.class, args);
+
   }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+
 }
