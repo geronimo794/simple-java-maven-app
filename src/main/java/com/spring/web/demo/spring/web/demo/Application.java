47bd96d1425e7419a6ff75bf0695683d695e7ef1
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-17 02:30:55 +0700

    test: Spring boot index

--- src/main/java/com/spring/web/demo/spring/web/demo/Application.java
+++ src/main/java/com/spring/web/demo/spring/web/demo/Application.java
@@ -2,15 +2,12 @@ package com.spring.web.demo.spring.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 
 @SpringBootApplication
-@RestController
-public class Application {
+public class ServingWebContentApplication {
+
     public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
+        SpringApplication.run(ServingWebContentApplication.class, args);
     }
+
 }
