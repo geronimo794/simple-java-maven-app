6b6a1b889ca3ec12f1e361cded91e7042b837296
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-17 01:19:09 +0700

    test: Fix application

--- src/main/java/com/spring/web/demo/spring/web/demo/Application.java
+++ src/main/java/com/spring/web/demo/spring/web/demo/Application.java
@@ -2,12 +2,18 @@ package com.spring.web.demo.spring.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
