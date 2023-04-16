15ccce0c604f815ff4431d8cfc058d8d505ff1be
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-04-17 02:47:23 +0700

    test: Rename controller

--- src/main/java/com/spring/web/demo/spring/web/demo/Application.java
+++ src/main/java/com/spring/web/demo/spring/web/demo/Application.java
@@ -1,21 +1,13 @@
 package com.spring.web.demo.spring.web.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.ui.Model;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
+
     public static void main(String[] args) {
-      SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
-    @GetMapping("/")
-	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "hello";
-	}
+
 }
